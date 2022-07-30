08846e95372e120f1c385c128230a57b2fe67fb5
Nightowlam <nightowl3.00am@gmail.com>
2022-07-30 23:38:15 +0600

    first commit

---
+++ variable-name.js
@@ -0,0 +1,37 @@
+
+
+// Right variable:
+var price = 40;
+
+//1. variable name can't be any keyword:
+var false = 96;
+var return = true;
+
+//2. variable name has to be one word. No space.
+var my home address = 'sundargonj gaibandha';
+
+//3. variable name can't have quotatin.
+var "age" = 32;
+var "name" = "ahmed jobayer";
+
+//4. variable name can't be start a number, but can ends with number.
+var 99club = 1997;
+var club99 = 1997;
+
+//variable name is case sensetive:
+var person = 25;
+var Person = 25; 
+
+//How to use long Name:
+var user-name = "bappa raj"; //can't use dash.
+var user_name = "bapaa raj"; //right 
+
+var user home address = "bd, dhaka"; // not right. 
+var userhomeaddress = "bd, dhaka"; //  right, but not perfect.
+var user_home_address = "bd, dhaka"; // snake case.
+var userHomeAddress = "bd, dhaka"; // camel case: we'll use this one.
+var UserHomeAddress = "bd, dhaka"; // pascal case.
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,25 @@
+
+// Types of Variabe:
+
+
+// Numeric:
+var price = 10;
+var age = 30;
+var temperature = 40;
+var money = 100;
+var weight = 60;
+var height = 2;
+var count = 99;
+var fair = 500;
+
+
+//String (dori):
+var name = "jobayer ahmed";
+var address = "sundargonj gaibandha";
+var wife = "tahmina xobayer";
+//Note: multi word er khetre single/double cotetion bosate hobe.
+
+//Boolean:
+var isSingle = false;
+var isSerious = true;
+var isSick = false;
